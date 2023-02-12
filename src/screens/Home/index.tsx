0b13c7f2780799cb90ee14a7852189ee31208f8b
Ronan Campos <ronannc1@yahoo.com.br>
2023-02-12 16:13:15 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Container} from './style';
+import {Text} from "react-native";
+export function Home() {
+  return <Container><Text>sdfasfa</Text></Container>;
+}
